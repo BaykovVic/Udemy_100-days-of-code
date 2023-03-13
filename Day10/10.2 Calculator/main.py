e6595816665ccb8a3a1c276258ed718e5b6cadbf
Victor <baykov.vic@hotmail.com>
2023-03-13 22:57:12 +0300

    Day10 - 10.2 Calculator refactoring

--- Day10/10.2 Calculator/main.py
+++ Day10/10.2 Calculator/main.py
@@ -1,14 +1,18 @@
 def add(number1, number2):
     return number1 + number2
 
+
 def subtract(number1, number2):
     return number1 - number2
+
 
 def multiply(number1, number2):
     return number1 * number2
 
+
 def divide(number1, number2):
     return number1 / number2
+
 
 operations = {
     "+": add,
@@ -17,14 +21,27 @@ operations = {
     "/": divide
 }
 
+from art import logo
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?"))
 
-num1 = int(input("What's the first number?"))
-num2 = int(input("What's the second number?"))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above")
-operation = operations[operation_symbol]
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above")
+        num2 = float(input("What's the second number?"))
+        operation = operations[operation_symbol]
+        result = operation(num1, num2)
 
-result = operation(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {result}")
+        print(f"{num1} {operation_symbol} {num2} = {result}")
+
+        if input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation:") == 'y':
+            num1 = result
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
