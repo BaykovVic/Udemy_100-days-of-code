5e99dced6f9f432d3b56c9e332617319374ca526
Victor <baykov.vic@hotmail.com>
2023-04-01 18:11:31 +0300

    Day21 - Refactoring usiin slicing

--- Day20/20.1 Snake/main.py
+++ Day20/20.1 Snake/main.py
@@ -32,10 +32,8 @@ while game_is_running:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_running = False
         scoreboard.game_over()
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_running = False
             scoreboard.game_over()
 
