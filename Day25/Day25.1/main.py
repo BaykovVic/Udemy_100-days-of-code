923deccdd9f45df3a6cfc3bdb1a19bd02b676750
Victor Baykov <vic.baykov89@gmail.com>
2023-07-18 11:28:39 +0300

    Day25 - DataFrames & Series. Working with rows and columns

--- Day25/Day25.1/main.py
+++ Day25/Day25.1/main.py
@@ -15,4 +15,39 @@ import pandas
 
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+#print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+avg_temp = data["temp"].mean()
+print(avg_temp)
+max_temp = data["temp"].max()
+print(max_temp)
+
+#Get data in columns
+print(data["condition"])
+print(data.condition)
+
+#Get data in row
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+monday_temp = int(monday.temp)
+monday_temp_F = monday_temp * 9 / 5 + 32
+print(monday_temp_F)
+
+#Create a dataframe from scratch
+data_dict = {
+    "students" : ["Amy", "James", "Angela"],
+    "scores" : [76, 56, 65]
+}
+
+df = pandas.DataFrame(data_dict)
+print(df)
+
+df.to_csv("new_data.csv")
