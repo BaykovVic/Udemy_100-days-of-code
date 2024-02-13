d9e0bba424b4541bdb172ed1aecbe7a22c7e42e2
victor <vic.baykov89@gmail.com>
2024-02-13 13:54:43 +0300

    Day 25.2 - The Great Squirrel Census Data Analysis

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/victor/Programming/Python/Lessons/Udemy_100-days-of-code/.venv"
+set -gx VIRTUAL_ENV "/home/victor/Programming/Python/Lessons/Udemy_100-days-of-code/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
