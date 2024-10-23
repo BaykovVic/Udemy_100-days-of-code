76914c9434bf293feb431d7f5d68678af0842b6a
victor <vic.baykov89@gmail.com>
2024-10-23 18:06:42 +0300

    Day 31.2 Flipping Cards Reading CSV file

--- Day31/Day31.1/main.py
+++ Day31/Day31.1/main.py
@@ -1,7 +1,17 @@
 import tkinter
 from tkinter import PhotoImage, Button
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card['French'])
 
 window = tkinter.Tk()
 window.title("Flashy")
@@ -10,22 +20,22 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = tkinter.Canvas(width=800, height=526)
 card_front_image = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_image)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cros_image = PhotoImage(file="images/wrong.png")
-unknown_button = tkinter.Button(image=cros_image, highlightthickness=0)
+unknown_button = tkinter.Button(image=cros_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = tkinter.Button(image=check_image, highlightthickness=0)
+known_button = tkinter.Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
 
 
 
 canvas.grid(row=0, column=0)
 
-
+next_card()
 window.mainloop()
