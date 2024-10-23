0b0f5b79bb6bcd5d4c644d2d280d00016eafe9dc
victor <vic.baykov89@gmail.com>
2024-10-23 18:58:55 +0300

    Day 31.3 Flipping Cards Saving progress

--- Day31/Day31.1/main.py
+++ Day31/Day31.1/main.py
@@ -1,13 +1,20 @@
 import tkinter
+from operator import index
 from tkinter import PhotoImage, Button
 import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 def next_card():
     global current_card, flip_timer
@@ -17,6 +24,12 @@ def next_card():
     canvas.itemconfig(card_word, text=current_card['French'], fill="black")
     canvas.itemconfig(card_background, image=card_front_image)
     flip_timer = window.after(3000, func=flip_card)
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 def flip_card():
@@ -44,7 +57,7 @@ unknown_button = tkinter.Button(image=cros_image, highlightthickness=0, command=
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = tkinter.Button(image=check_image, highlightthickness=0, command=next_card)
+known_button = tkinter.Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 
