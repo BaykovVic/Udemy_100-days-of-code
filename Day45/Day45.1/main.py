279b804092095aae7c02008adc38280043284a0e
victor <vic.baykov89@gmail.com>
2024-11-01 14:58:26 +0300

    Day 45.2 Finding and Selecting Particular Elements (bs4)

--- Day45/Day45.1/main.py
+++ Day45/Day45.1/main.py
@@ -6,3 +6,23 @@ with open("website.html") as file:
 
 soup = BeautifulSoup(content, "html.parser")
 print(soup.title.string)
+all_anchor_tags = soup.find_all(name="a")
+print(all_anchor_tags)
+
+for tag in all_anchor_tags:
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading.get_text())
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.get_text())
+
+company_url = soup.select_one(selector="p a")
+print(company_url.get("href"))
+
+name = soup.select_one(selector="#name")
+print(name.get_text())
+
+headings = soup.select(".heading")
+print(headings)
